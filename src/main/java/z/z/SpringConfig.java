67b1c6c62778574c7378916ef3754c44fb1f8e38
PanLiang <panliang@foda.ai>
2023-11-30 16:44:10 +0800

    commit code

--- src/main/java/z/z/SpringConfig.java
+++ src/main/java/z/z/SpringConfig.java
@@ -2,12 +2,7 @@ package z.z;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cache.interceptor.SimpleKeyGenerator;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -25,9 +19,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 @RestController
 @EnableWebMvc // 开启webmvc
@@ -37,7 +28,7 @@ import java.util.concurrent.Executors;
 @EnableAsync //启动异步任务
 @EnableAspectJAutoProxy // 开启spring aop
 @EnableCaching //开启spring 缓存
-public class SpringConfig implements WebMvcConfigurer, SchedulingConfigurer {
+public class SpringConfig implements WebMvcConfigurer {
 
 
     @Override
@@ -56,16 +47,6 @@ public class SpringConfig implements WebMvcConfigurer, SchedulingConfigurer {
         return converter;
     }
 
-    @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        taskRegistrar.setScheduler(taskSExecutor());
-    }
-
-    @Bean
-    public Executor taskSExecutor() {
-        return Executors.newScheduledThreadPool(8);
-    }
-
     /**
      * 配置默认的加载返回
      * @return JSONObject
@@ -77,5 +58,18 @@ public class SpringConfig implements WebMvcConfigurer, SchedulingConfigurer {
         jsonObject.put("name,", "ssm6");
         jsonObject.put("version,", "1.0");
         return jsonObject;
+    }
+
+    /**
+     * 线程池任务执行程序
+     * @return ThreadPoolTaskExecutor
+     */
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor () {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5); // 核心线程为5
+        taskExecutor.setMaxPoolSize(10); // 最大线程为10
+        taskExecutor.setQueueCapacity(25); // 设置队列容量
+        return taskExecutor;
     }
 }
