ed3e0ccc51dbdf7be7f50829304e0769a8e03b66
PanLiang <panliang@foda.ai>
2023-12-06 11:17:19 +0800

    commit code

--- src/main/java/z/z/config/SpringMybatisConfig.java
+++ src/main/java/z/z/config/SpringMybatisConfig.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -17,6 +18,19 @@ import javax.sql.DataSource;
 @MapperScan("z.z.mapper")
 public class SpringMybatisConfig {
 
+    @Value("${hikariCP.driverClassName}")
+    private String driverClassName;
+    @Value("${hikariCP.jdbcUrl}")
+    private String jdbcUrl;
+    @Value("${hikariCP.userName}")
+    private String userName;
+    @Value("${hikariCP.password}")
+    private String password;
+    @Value("${hikariCP.autoCommit}")
+    private Boolean autoCommit;
+    @Value("${hikariCP.poolName}")
+    private String poolName;
+
     /**
      * 配置数据源
      * @return DataSource
@@ -24,18 +38,12 @@ public class SpringMybatisConfig {
     @Bean
     public HikariDataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/ppap");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-
-        dataSource.setAutoCommit(Boolean.TRUE); //自动提交事务
-        dataSource.setConnectionTimeout(180 * 1000); //连接超时时间
-        dataSource.setIdleTimeout(30 * 1000); //闲置后抛弃
-        dataSource.setKeepaliveTime(0);// 检查一次连接的活性
-        dataSource.setMinimumIdle(5);// 核心连接数
-        dataSource.setMaximumPoolSize(10); // 最大连接数
-        dataSource.setPoolName("数据库连接池-");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setAutoCommit(autoCommit);
+        dataSource.setPoolName(poolName);
         return dataSource;
     }
 
